001ae2f90a80735f160084347db0a08f2d61f5ce
Arseniy Gaintsev <arseniy.gaintsev@gmail.com>
2021-11-24 16:24:54 +0300

    allowed_hosts changes

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'cats.arscorp.ru',
+    'www.cats.arscorp.ru',
+    '51.250.27.55',
+]
 
 
 # Application definition
